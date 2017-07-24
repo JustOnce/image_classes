944f4dbb31335a08ea70b137707eec8ca5784cae
Denis Nikanorov <dnikanorov@benovate.ru>
2017-07-24 13:14:23 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_version(*file_paths):
     raise RuntimeError('Unable to find version string.')
 
 
-version = get_version('mommy_spatial_generators', '__init__.py')
+version = get_version('image_classes', '__init__.py')
 
 
 if sys.argv[-1] == 'publish':
